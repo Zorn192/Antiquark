6439896c3c8051cceb130d5363859511fb0105c1
Zeker0 <zorn@hotmail.co.uk>
2018-05-06 19:31:41 +0100

    Create .user.js

---
+++ .user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Antiquark-Zorn192
+// @version      1.1
+// @namespace    https://Zorn192.github.io/Antiquark
+// @updateURL    https://Zorn192.github.io/Antiquark/.user.js
+// @description  Automate Antimatter dimensions
+// @include      *ivark.github.io*
+// @connect      *Zorn192.github.io/Antiaquark*
+// @connect      *ivark.github.io*
+// @connect      self
+// @grant        none
+// ==/UserScript==
+
+var script = document.createElement('script');
+script.id = 'Antiquark-script';
+script.src = 'https://Zorn192.github.io/Antiquark/Antiquark.js';
+script.setAttribute('crossorigin',"anonymous");
+document.head.appendChild(script);
